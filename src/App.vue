2d26f96541907498ccbde6468e57c147a570de46
Donatella7 <campaniellodonatella@gmail.com>
2022-04-07 13:02:44 +0200

    prova img jumbo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <HeaderWrap/>
+    <img class="hero" :src="require('./assets/img/hero-07-2x.jpg')" alt="">
   </div>
 </template>
 
@@ -15,5 +16,9 @@ export default {
 </script>
 
 <style lang="scss">
-
+.hero{
+  width:100%;
+  height: 600px;
+  object-fit: center;
+}
 </style>
