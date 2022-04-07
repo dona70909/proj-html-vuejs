c15e092d4a073e19a7e6deb3b1838c9b2eafd08b
Donatella7 <campaniellodonatella@gmail.com>
2022-04-07 18:21:06 +0200

    audible section done

--- src/components/main/AudibleAd.vue
+++ src/components/main/AudibleAd.vue
@@ -1,14 +1,15 @@
 <template>
     <section class="container-fluid bg-ad-audible">
         <div class="row px-5 justify-content-center">
-            <div class="col-6 py-4">
-                <div class="w-50">
+            <div class="col-8 d-flex justify-content-center clearfix">
+                <div class="w-50 float-end d-flex justify-content-end">
                     <img :src="require('@/assets/img/audible-app-2x-400x418.png')" alt="">
                 </div>
-            </div>
-            <div class="col-6 text-warning py-4">
-                <h5>New Audiobook</h5>
-                <h6>No time to read? No problem. Listen to it.</h6>
+                <div class="w-50 float-end text-warning d-flex flex-column align-content-center justify-content-center">
+                    <h5>New Audiobook</h5>
+                    <h6>No time to read? No problem. Listen to it.</h6>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quo, praesentium eaque sint in consequatur!</p>
+                </div>
             </div>
         </div>
     </section>
