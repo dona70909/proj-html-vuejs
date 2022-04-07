72e2878d113e318b371a14ae5bc0e7e2c1ac292a
Donatella7 <campaniellodonatella@gmail.com>
2022-04-07 17:33:16 +0200

    audible backround image

---
+++ src/components/main/AudibleAd.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="container-fluid bg-ad-audible">
+        <div class="row px-5">
+            <div class="col-6"></div>
+            <div class="col-6"></div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name:"AudibleAd",
+
+}
+</script>
+
+<style lang="scss" scoped>
+.bg-ad-audible{
+    background-image: url('@/assets/img/banner.jpg');
+    height:300px;
+    background-repeat: no-repeat;
+    background-position: 50% 30%;
+   /*  background: black; */
+}
+</style>
--- src/components/main/MainWrap.vue
+++ src/components/main/MainWrap.vue
@@ -1,15 +1,18 @@
 <template>
     <main>
         <MoreInfo/>
+        <AudibleAd/>
     </main>
 </template>
 
 <script>
 import MoreInfo from './MoreInfo.vue'
+import AudibleAd from './AudibleAd.vue'
 export default {
     name:"MainWrap",
     components:{
         MoreInfo,
+        AudibleAd,
     }
 }
 </script>
