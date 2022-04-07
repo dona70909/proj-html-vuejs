6c3227bdcb69e677a421e1444728987a3dbb7a7a
Donatella7 <campaniellodonatella@gmail.com>
2022-04-07 17:39:50 +0200

    audible

--- src/components/main/AudibleAd.vue
+++ src/components/main/AudibleAd.vue
@@ -1,8 +1,15 @@
 <template>
     <section class="container-fluid bg-ad-audible">
-        <div class="row px-5">
-            <div class="col-6"></div>
-            <div class="col-6"></div>
+        <div class="row px-5 justify-content-center">
+            <div class="col-6 py-4">
+                <div class="w-50">
+                    <img :src="require('@/assets/img/audible-app-2x-400x418.png')" alt="">
+                </div>
+            </div>
+            <div class="col-6 text-warning py-4">
+                <h5>New Audiobook</h5>
+                <h6>No time to read? No problem. Listen to it.</h6>
+            </div>
         </div>
     </section>
 </template>
@@ -17,9 +24,12 @@ export default {
 <style lang="scss" scoped>
 .bg-ad-audible{
     background-image: url('@/assets/img/banner.jpg');
-    height:300px;
+    
     background-repeat: no-repeat;
     background-position: 50% 30%;
-   /*  background: black; */
+}
+
+img{
+    height: 300px;
 }
 </style>
