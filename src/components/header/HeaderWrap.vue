527926afc10233c1a0543e0d75384274b452df70
Donatella7 <campaniellodonatella@gmail.com>
2022-04-07 12:57:33 +0200

    skeleton header

--- src/components/header/HeaderWrap.vue
+++ src/components/header/HeaderWrap.vue
@@ -1,6 +1,23 @@
 <template>
     <header>
-        <h1 class="text-danger">Header</h1>
+        <section class="container-fluid">
+            <div class="row align-items-center justify-content-between px-5">
+                <div class="col-6">
+                    <img :src="require('@/assets/img/author-logo-round-small.png')" alt="logo header">
+                </div>
+                <nav class="col-6">
+                    <ul class="d-flex justify-content-evenly">
+                        <li><a href="#/">About me</a></li>
+                        <li><a href="#/">Testimonials</a></li>
+                        <li><a href="#/">My Dig</a></li>
+                        <li><a href="#/">Meetups</a></li>
+                        <li><a href="#/">Shop</a></li>
+                        <li><a href="#/">Contact me</a></li>
+                        <li>icon</li>
+                    </ul>
+                </nav>
+            </div>
+        </section>
     </header>
 </template>
 
@@ -11,5 +28,17 @@ name:"HeaderWrap"
 </script>
 
 <style lang="scss" scoped>
+header{
+    background: black;
+    color:white;
 
+    ul{
+        list-style: none;
+
+        a{
+            color:lightgrey;
+            text-decoration: none;
+        }
+    }
+}
 </style>
