358da49a9b4f701bb5b9b1a7723e688fc4e33bfb
Donatella7 <campaniellodonatella@gmail.com>
2022-04-07 18:29:43 +0200

    logos audible add section

--- src/components/main/AudibleAd.vue
+++ src/components/main/AudibleAd.vue
@@ -3,12 +3,17 @@
         <div class="row px-5 justify-content-center">
             <div class="col-8 d-flex justify-content-center clearfix">
                 <div class="w-50 float-end d-flex justify-content-end">
-                    <img :src="require('@/assets/img/audible-app-2x-400x418.png')" alt="">
+                    <img class="audible-screen" :src="require('@/assets/img/audible-app-2x-400x418.png')" alt="">
                 </div>
                 <div class="w-50 float-end text-warning d-flex flex-column align-content-center justify-content-center">
                     <h5>New Audiobook</h5>
                     <h6>No time to read? No problem. Listen to it.</h6>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quo, praesentium eaque sint in consequatur!</p>
+                    <div class="logos-container d-flex justify-content-evenly">
+                        <img :src="require('@/assets/img/audible-logo.png')" alt="logo audible">
+                        <img :src="require('@/assets/img/bn-logo-tall.png')" alt="barnes">
+                        <img :src="require('@/assets/img/kindle-logo-tall.png')" alt="kindle">
+                    </div>
                 </div>
             </div>
         </div>
@@ -30,7 +35,7 @@ export default {
     background-position: 50% 30%;
 }
 
-img{
+.audible-screen{
     height: 300px;
 }
 </style>
