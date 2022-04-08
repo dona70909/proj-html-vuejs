a0aed1bb00f1ad35e6095aff12f2247dc4e6a879
Donatella7 <campaniellodonatella@gmail.com>
2022-04-08 10:54:11 +0200

    copyright

--- src/components/footer/FooterCopyright.vue
+++ src/components/footer/FooterCopyright.vue
@@ -1,8 +1,8 @@
 <template>
     <section class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                copy
+        <div class="row justify-content-center">
+            <div class="col-7 copyright-container text-white text-center">
+                <p> <img :src="require('@/assets/img/copyright-icon.png')" alt="copyright logo">Copyright 2012-2020 | Avada Theme by ThemeFusion | All Rights Reserved | Powered by Wordpress</p>
             </div>
         </div>
     </section>
@@ -15,5 +15,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    
+.copyright-container{
+    padding-top: 13rem;
+
+    img{
+        width:1.2rem;
+        color:white;
+    } 
+}
+
 </style>
