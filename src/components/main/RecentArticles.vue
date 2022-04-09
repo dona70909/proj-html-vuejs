b1bb4cccc37c6d51937c753d41601c9be50a05f4
Donatella7 <campaniellodonatella@gmail.com>
2022-04-09 17:08:46 +0200

    recent article section

--- src/components/main/RecentArticles.vue
+++ src/components/main/RecentArticles.vue
@@ -15,16 +15,24 @@
                     <img :src="mainArticle.img" :alt="mainArticle.title">
                     <div class="text-big-box">
                         <h5>{{mainArticle.title}}</h5>
-                        <h6 class="mb-4">{{mainArticle.subtitle}}</h6>
-                        <p>{{mainArticle.paragraph}}</p>
+                        <!-- <h6 class="mb-4">{{mainArticle.subtitle}}</h6> -->
+                        <div class="mb-3">
+                            <span class="gray-text">By </span><span>{{mainArticle.author}}</span>
+                            <span class="gray-text">{{' | ' + mainArticle.date + ' | '}}</span>
+                            <span class="gray-text">Categories: </span><span>{{mainArticle.categories + ' | '}}</span>
+                        </div>
+                        <p class="gray-text">{{mainArticle.paragraph}}</p>
                     </div>
                 </div>
             </div>
             <div class="col-3">
-                <div v-for="(article,index) in recentArticles" :key="index + 'recent' " class="pt-5 small-box d-flex flex-column justify-content-center">
-                    <h4 class="pb-2">{{article.title}}</h4>
-                    <img :src="article.img" alt="">
-                    <p class="pb-5 m-0">{{article.paragraph}}</p>
+                <div v-for="(article,index) in recentArticles" :key="index + 'recent' " class="small-box d-flex flex-column justify-content-center">
+                    <h4 class="">{{article.title}}</h4>
+                    <img :src="article.img" :alt="article.title">
+                    <div class="mb-0">
+                        <span class="gray-text">{{article.date + ' |'}} </span>
+                        <span>{{article.comments + ' |'}}</span>                        
+                    </div>
                 </div>
             </div>
         </div>
@@ -40,11 +48,17 @@ export default {
             recentArticles:[
                 {
                     title:"What's on your booklist",
+                    comments:"0 comments",
+                    author:"admin",
+                    date:"October 24th 2019",
                     img:require('@/assets/img/thumb-13-2x-320x202.jpg'),
                     paragraph:"October 24th, 2019 | 0 Comments"
                 },
                 {
                     title:"Istanbul travel guide",
+                    comments:"0 comments",
+                    author:"admin",
+                    date:"October 24th 2019",
                     img:require('@/assets/img/thumb-08-2x-320x202.jpg'),
                     paragraph:"October 24th, 2019 | 0 Comments"
                 }
@@ -52,7 +66,9 @@ export default {
 
             mainArticle:{
                 title:"C20 Creativity Tips",
-                subtitle:"By admin| October 24th 2019| Categories: Event,Tips and Tricks",
+                categories:"Event,Tips & Tricks",
+                author:"admin",
+                date:"October 24th 2019",
                 img:require('@/assets/img/20-Tips-400x235.jpg'),
                 paragraph:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod officia non sit error! Praesentium, magnam. Velit quod esse, similique laboriosam."
             }
@@ -86,7 +102,7 @@ p {
         font-weight: 600;
     }
 
-    button{
+    button {
         background-color: #f4b429;
         border:none;
         padding: .5rem 3.6rem;
@@ -109,6 +125,10 @@ p {
 .wrapper-articles {
     padding-bottom: 6rem;
 
+    .gray-text {
+        color:$osloGray;
+    }
+
     .big-box {
         background-color:white;
         padding: 2rem;
@@ -118,17 +138,27 @@ p {
             width:100%;
             margin-bottom: 2rem;
         }
+
+        span {
+            font-size:.9rem;
+        }
     }
 
     .small-box {
         background-color:white;
-        padding-left:2rem;
-        padding-right: 2rem;
         height: 50%;
-        
+        padding:1.3rem 2rem;
         img {
             width:100%;
-            margin-bottom: 2rem;
+            margin-bottom: 1rem;
+        }
+
+        p {
+            padding-bottom: 1rem;
+        }
+
+        span {
+            font-size:.9rem;
         }
 
     }
