b1e1697804e442d5a104fbd4016b092e345712f2
Donatella7 <campaniellodonatella@gmail.com>
2022-04-10 17:42:57 +0200

    audible section  with d-flex instead of clearfix

--- src/components/main/AudibleAd.vue
+++ src/components/main/AudibleAd.vue
@@ -1,19 +1,19 @@
 <template>
     <section class="container-fluid bg-ad-audible">
         <div class="row px-5 pt-4 justify-content-center">
-            <div class="col-8 d-flex justify-content-center clearfix">
+            <div class="col-6 d-flex justify-content-end">
                 <div class="w-50 float-end d-flex justify-content-end">
                     <img class="audible-screen" :src="require('@/assets/img/audible-app-2x-400x418.png')" alt="">
                 </div>
-                <div class="container-article text-center w-50 float-end d-flex flex-column align-content-center justify-content-center">
-                    <h5 class="title-article">New Audiobook</h5>
-                    <h6 class="subtitle-article">No time to read? No problem. Listen to it.</h6>
-                    <p class="paragraph-article">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quo, praesentium eaque sint in consequatur!</p>
-                    <div class="logos-container d-flex justify-content-evenly">
-                        <img :src="require('@/assets/img/audible-logo.png')" alt="logo audible">
-                        <img :src="require('@/assets/img/bn-logo-tall.png')" alt="barnes">
-                        <img :src="require('@/assets/img/kindle-logo-tall.png')" alt="kindle">
-                    </div>
+            </div>
+            <div class="col-6 d-flex flex-column align-items-center container-article align-self-center">
+                <h5 class="title-article">New Audiobook</h5>
+                <h6 class="subtitle-article">No time to read? No problem. Listen to it.</h6>
+                <p class="paragraph-article text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quo, praesentium eaque sint in consequatur!</p>
+                <div class="logos-container d-flex justify-content-between">
+                    <img class="img-fluid" :src="require('@/assets/img/audible-logo.png')" alt="logo audible">
+                    <img class="img-fluid" :src="require('@/assets/img/bn-logo-tall.png')" alt="barnes">
+                    <img class="img-fluid" :src="require('@/assets/img/kindle-logo-tall.png')" alt="kindle">
                 </div>
             </div>
         </div>
@@ -40,13 +40,12 @@ export default {
 .audible-screen {
     height: 300px;
 }
-
 .container-article {
 
     .title-article {
-        color: white;
-        font-family: 'PT Serif', serif;
-        font-size: 2.2rem;
+    color: white;
+    font-family: 'PT Serif', serif;
+    font-size: 2.2rem;
     }
 
     .subtitle-article {
@@ -57,5 +56,13 @@ export default {
         color:$lightGray;
         font-family: 'PT Serif', serif;
     }
+
+    .container-logos {
+        width: 100%;
+        img {
+            width: calc(100% / 3);
+        }
+    }
 }
+
 </style>
