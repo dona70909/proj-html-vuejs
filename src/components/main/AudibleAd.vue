582da402ce61614ce423e04d5b357bfabba62c35
Donatella7 <campaniellodonatella@gmail.com>
2022-04-10 19:51:19 +0200

    audible sec

--- src/components/main/AudibleAd.vue
+++ src/components/main/AudibleAd.vue
@@ -1,19 +1,19 @@
 <template>
     <section class="container-fluid bg-ad-audible">
         <div class="row px-5 pt-4 justify-content-center">
-            <div class="col-6 d-flex justify-content-end">
-                <div class="w-50 float-end d-flex justify-content-end">
-                    <img class="audible-screen" :src="require('@/assets/img/audible-app-2x-400x418.png')" alt="">
-                </div>
+            <div class="col-6 px-0 d-flex justify-content-end">
+                <img class="audible-screen" :src="require('@/assets/img/audible-app-2x-600x628.png')" alt="">
             </div>
-            <div class="col-6 d-flex flex-column align-items-center container-article align-self-center">
-                <h5 class="title-article">New Audiobook</h5>
-                <h6 class="subtitle-article">No time to read? No problem. Listen to it.</h6>
-                <p class="paragraph-article text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quo, praesentium eaque sint in consequatur!</p>
-                <div class="logos-container d-flex justify-content-between">
-                    <img class="img-fluid" :src="require('@/assets/img/audible-logo.png')" alt="logo audible">
-                    <img class="img-fluid" :src="require('@/assets/img/bn-logo-tall.png')" alt="barnes">
-                    <img class="img-fluid" :src="require('@/assets/img/kindle-logo-tall.png')" alt="kindle">
+            <div class="col-6 px-0 container-article align-self-center text-white text-center">
+                <div class="text-article w-75">
+                    <h5 class="title-article">New Audiobook</h5>
+                    <h6 class="subtitle-article">No time to read? No problem. Listen to it.</h6>
+                    <p class="paragraph-article text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quo, praesentium eaque sint in consequatur!</p>
+                </div>
+                <div class="container-logos d-flex justify-content-between w-75">
+                    <img class="mx-2 img-fluid" :src="require('@/assets/img/audible-logo.png')" alt="logo audible">
+                    <img class="mx-2 img-fluid"   :src="require('@/assets/img/bn-logo-tall.png')" alt="barnes">
+                    <img  class="mx-2 img-fluid"  :src="require('@/assets/img/kindle-logo-tall.png')" alt="kindle">
                 </div>
             </div>
         </div>
@@ -57,12 +57,10 @@ export default {
         font-family: 'PT Serif', serif;
     }
 
-    .container-logos {
-        width: 100%;
-        img {
-            width: calc(100% / 3);
-        }
+    img {
+        width: 50%;
     }
+
 }
 
 </style>
