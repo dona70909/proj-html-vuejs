fa9fb7299930f86777c341c31c08bb5965193468
Donatella7 <campaniellodonatella@gmail.com>
2022-04-10 20:23:18 +0200

    audible ad

--- src/components/main/AudibleAd.vue
+++ src/components/main/AudibleAd.vue
@@ -5,12 +5,12 @@
                 <img class="audible-screen" :src="require('@/assets/img/audible-app-2x-600x628.png')" alt="">
             </div>
             <div class="col-6 px-0 container-article align-self-center text-white text-center">
-                <div class="text-article w-75">
+                <div class="text-article">
                     <h5 class="title-article">New Audiobook</h5>
                     <h6 class="subtitle-article">No time to read? No problem. Listen to it.</h6>
                     <p class="paragraph-article text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quo, praesentium eaque sint in consequatur!</p>
                 </div>
-                <div class="container-logos d-flex justify-content-between w-75">
+                <div class="container-logos d-flex justify-content-between">
                     <img class="mx-2 img-fluid" :src="require('@/assets/img/audible-logo.png')" alt="logo audible">
                     <img class="mx-2 img-fluid"   :src="require('@/assets/img/bn-logo-tall.png')" alt="barnes">
                     <img  class="mx-2 img-fluid"  :src="require('@/assets/img/kindle-logo-tall.png')" alt="kindle">
@@ -51,15 +51,27 @@ export default {
     .subtitle-article {
         color:$saffronOrangeGold;
         text-transform: uppercase;
+        font-size: .8rem;
     }
     .paragraph-article {
         color:$lightGray;
         font-family: 'PT Serif', serif;
+        font-size: .7rem;
     }
 
     img {
         width: 50%;
     }
+
+    .text-article {
+        width:50%;
+    }
+
+    .container-logos {
+        width: 50%;
+    }
+
+
 
 }
 
